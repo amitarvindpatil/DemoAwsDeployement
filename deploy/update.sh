dd6c57aa13821683ab62682a9e75c9b46a2cd1bd
amitarvindpatil <amitpatil04041993@gmail.com>
2021-01-31 17:23:10 +0530

    add urls

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/demoproject'
 
 git pull
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+$PROJECT_BASE_PATH/env/bin/python3 manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python3 manage.py collectstatic --noinput
+supervisorctl restart demoproject
 
 echo "DONE! :)"
